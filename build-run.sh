afc3a1306a451b1ef2dca5ab779aa96667b3f67d
aaronhackney <ahack210@gmail.com>
2021-02-12 19:39:34 -0600

    cleanup

--- build-run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-/usr/local/bin/yarn workspace frontend build
-/usr/local/bin/yarn workspace backend clean
-/usr/local/bin/yarn workspace backend build
-/usr/local/bin/yarn workspace backend start-prod
-
